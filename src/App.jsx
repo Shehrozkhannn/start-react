a2056dcfa5e21e61d90e8d9293a127d99af1e67d
Saad.Q <saad.q@simplifyvms.com>
2024-11-23 18:41:12 +0500

    Create background generator in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
+import { useState } from 'react';
 import './App.css';
-import Card from './components/Card';
 
 function App() {
 
+  const [color, setColor] = useState("olive");
+  
+const buttonColors = [
+  { name: "Red", backgroundColor: "red" },
+  { name: "Blue", backgroundColor: "blue" },
+  { name: "Green", backgroundColor: "green" },
+  { name: "Pink", backgroundColor: "pink" },
+];
+
   return (
     <>
-      <h2 className='bg-green-500'>CARD PROPS</h2>
-      <Card channel='sharoze works' age='20'/>
+    <div className='w-full h-screen duration-200' style={{backgroundColor: color}}>
+
+      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+        <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
+          {
+            buttonColors.map((btn,index)=> (
+              <button key={index} className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+              style={{background: btn.backgroundColor}}
+              onClick={() => setColor(btn.backgroundColor)}
+              >
+                  {btn.name}
+              </button>
+            ))
+          }
+        </div>
+      </div>
+    </div>
     </>
   );
 }
