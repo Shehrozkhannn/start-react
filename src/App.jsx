8b4f554db5bef951c23011d30a42d67d7b61f123
Saad.Q <saad.q@simplifyvms.com>
2024-11-23 18:07:17 +0500

    Implement tailwind with react

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Card from './components/Card';
+
+function App() {
+
+  return (
+    <>
+      <h2 className='bg-green-500'>CARD PROPS</h2>
+      <Card channel='sharoze works' age='20'/>
+    </>
+  );
+}
+
+export default App;
